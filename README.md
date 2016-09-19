83e3fe7f0b84f1fdad5a95a84fc681b2cd29b915
Bruce Marriner <bruce@sqls.net>
2016-09-19 05:52:10 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# sql2xlsx
+sql2xlsx
+====
+[![Go report](http://goreportcard.com/badge/bwmarrin/sql2xlsx)](http://goreportcard.com/report/bwmarrin/sql2xlsx) [![Discord Gophers](https://img.shields.io/badge/Discord%20Gophers-%23info-blue.svg)](https://discord.gg/0f1SbxBZjYq9jLBk)
+====
 A simple program to convert SQL rows into Microsoft Excel XLSX files.
+
+This example is built to work with MS SQL driver however it can easily be 
+modified to function with any other Go SQL driver.
+
+**For help with this package or general Go discussion, please join the [Discord 
+Gophers](https://discord.gg/0f1SbxBZjYq9jLBk) chat server.**
+
+## Install & Build
+
+This assumes you already have a working Go environment, if not please see
+[this page](https://golang.org/doc/install) first.
+
+```sh
+go get https://github.com/bwmarrin/sql2xlsx.git
+cd sql2xlsx
+go build
+```
+
+## Usage
+
+All options except for are required.
+
+```
+Usage of ./sql2xlsx:
+  -h string
+        SQL Server hostname or IP
+  -u string
+        User ID
+  -p string
+        Password
+  -s string
+        SQL Query filename
+  -o string
+        Output filename
+```
+
---
+++ main.go
@@ -0,0 +1,153 @@
+// A simple example of quickly converting SQL result into an Excel file.
+package main
+
+import (
+	"database/sql"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"time"
+
+	_ "github.com/denisenkom/go-mssqldb"
+	"github.com/tealeg/xlsx"
+)
+
+// Variables used with command line arguments
+var (
+	host string
+	user string
+	pass string
+	sqlf string
+	outf string
+)
+
+// Parse command line arguments
+func init() {
+
+	flag.StringVar(&host, "h", "", "SQL Server hostname or IP")
+	flag.StringVar(&user, "u", "", "User ID")
+	flag.StringVar(&pass, "p", "", "Password")
+	flag.StringVar(&sqlf, "s", "", "SQL Query filename")
+	flag.StringVar(&outf, "o", "", "Output filename")
+
+	if len(os.Args) < 5 {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	flag.Parse()
+
+}
+
+func main() {
+
+	// Connect to SQL Server
+	dsn := fmt.Sprintf("server=%s;user id=%s;password=%s;encrypt=disable", host, user, pass)
+	conn, err := sql.Open("mssql", dsn)
+	if err != nil {
+		log.Fatalf("error opening connection to server, %s\n", err)
+	}
+	defer conn.Close()
+
+	// Verify connection to SQL Server
+	err = conn.Ping()
+	if err != nil {
+		log.Fatalf("error verifying connection to server, %s\n", err)
+		return
+	}
+
+	// Load SQL Query file
+	query, err := ioutil.ReadFile(sqlf)
+	if err != nil {
+		log.Fatalf("error opening SQL Query file %s, %s\n", sqlf, err)
+	}
+
+	// Query the database
+	rows, err := conn.Query(string(query))
+	if err != nil {
+		log.Fatalf("error running query, %s\n", err)
+	}
+	defer rows.Close()
+
+	err = generateXLSXFromRows(rows, outf)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func generateXLSXFromRows(rows *sql.Rows, outf string) error {
+
+	var err error
+
+	// Get column names from query result
+	colNames, err := rows.Columns()
+	if err != nil {
+		return fmt.Errorf("error fetching column names, %s\n", err)
+	}
+	length := len(colNames)
+
+	// Create a interface slice filled with pointers to interface{}'s
+	pointers := make([]interface{}, length)
+	container := make([]interface{}, length)
+	for i := range pointers {
+		pointers[i] = &container[i]
+	}
+
+	// Create output xlsx workbook
+	xfile := xlsx.NewFile()
+	xsheet, err := xfile.AddSheet("Sheet1")
+	if err != nil {
+		return fmt.Errorf("error adding sheet to xlsx file, %s\n", err)
+	}
+
+	// Write Headers to 1st row
+	xrow := xsheet.AddRow()
+	xrow.WriteSlice(&colNames, -1)
+
+	// Process sql rows
+	for rows.Next() {
+
+		// Scan the sql rows into the interface{} slice
+		err = rows.Scan(pointers...)
+		if err != nil {
+			return fmt.Errorf("error scanning sql row, %s\n", err)
+		}
+
+		xrow = xsheet.AddRow()
+
+		// Here we range over our container and look at each column
+		// and set some different options depending on the column type.
+		for _, v := range container {
+			xcell := xrow.AddCell()
+			switch v.(type) {
+			case string:
+				xcell.SetString(v.(string))
+			case []byte:
+				xcell.SetString(v.(string))
+			case int64:
+				xcell.SetInt64(v.(int64))
+			case float64:
+				xcell.SetFloat(v.(float64))
+			case bool:
+				xcell.SetBool(v.(bool))
+			case time.Time:
+				xcell.SetDateTime(v.(time.Time))
+			default:
+				xcell.SetValue(v)
+			}
+
+		}
+
+	}
+
+	// Save the excel file to the provided output file
+	err = xfile.Save(outf)
+	if err != nil {
+		return fmt.Errorf("error writing to output file %s, %s\n", outf, err)
+	}
+
+	return nil
+}
